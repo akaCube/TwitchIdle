85d12944406ecb98bbe2268ce70cd78228d259f1
akaCube <dennnis1995@live.de>
2016-06-27 13:26:07 +0200

    added external functions for clicking

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 // HELP FUNCTIONS //
 ////////////////////
 
-function semiround(input, place){
+function l(input, place){
   var placer = Math.pow(10, place);
   var output = Math.round(input * placer)/placer;
   return output;
@@ -59,6 +59,21 @@ Game.Load = function(){
 
   // Actions //
 
+  // Functions //
+
+  Game.ClickMoney = function(){
+    Game.AddMoney(0.01);
+  };
+  Game.ClickEmote = function(){
+    Game.AddEmote(1);
+  };
+  Game.AddMoney = function(amount){
+    Game.money = l(Game.money + amount, 2);
+  }
+  Game.AddEmote = function(amount){
+    Game.emotes += amount
+  }
+
 }
 
 Game.Load();
@@ -75,18 +90,6 @@ var vm = new Vue({
   methods:{
     cycle: function(){
       
-    },
-    clickMoney: function(){
-      this.addMoney(0.01);
-    },
-    clickKappa: function(){
-      this.addEmotes(1);
-    },
-    addMoney: function(amount){
-      Game.money = semiround(Game.money + amount, 2);
-    },
-    addEmotes: function(amount){
-      Game.emotes += amount;
     },
     buyCursor: function(){
       var viewerCost = Math.floor(1 * Math.pow(1.1,Game.viewers));
